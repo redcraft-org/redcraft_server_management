f11082924857a1271530fd2e9d0a2e22ea7c3271
lululombard <lululombard@gmail.com>
2020-08-30 01:03:39 -0400

    Set vscode settings in the build

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ set -e
 export GOPATH=`pwd`
 export GO_EXECUTABLE=`which go`
 
+# Set vscode settings
+mkdir -p .vscode
+echo -n "{
+    \"go.gopath\": \"`pwd`\"
+}" > .vscode/settings.json
+
 "$GO_EXECUTABLE" get github.com/wricardo/gomux
 "$GO_EXECUTABLE" get github.com/joho/godotenv
 "$GO_EXECUTABLE" get github.com/go-redis/redis
